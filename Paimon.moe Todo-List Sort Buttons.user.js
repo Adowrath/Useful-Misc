17d87f0247be9e510639b86fba67bba6f6836f29
Adowrath <cyrill.brunner@hispeed.ch>
2023-02-14 17:05:15 +0100

    Cleaned up and organised a bit

--- Paimon.moe Todo-List Sort Buttons.user.js
+++ Paimon.moe Todo-List Sort Buttons.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Sort Buttons for Todo List
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Adds Sort Buttons to the Paimon.moe Todo List that can sort the list entries by increasing
 // @author       Adowrath
 // @match        https://paimon.moe/*
@@ -9,8 +9,26 @@
 // @grant        none
 // ==/UserScript==
 
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+const THRESHOLD_LOG_CACHE_LIMIT = 2;
+let thresholdLogCache = [];
+function tLog(...args) {
+    if(thresholdLogCache?.length < THRESHOLD_LOG_CACHE_LIMIT) {
+        thresholdLogCache.push(args);
+        return;
+    }
+    if(thresholdLogCache?.length >= THRESHOLD_LOG_CACHE_LIMIT) {
+        for(let cachedArgs of thresholdLogCache) {
+            console.log(...cachedArgs);
+        }
+        thresholdLogCache = undefined;
+    }
+    console.log(...args);
+}
+function restoreTLogCache() { thresholdLogCache = []; }
+
 async function sortCharacters() {
-    const sleep = (ms) => new Promise(r => setTimeout(r, ms));
     let itemTexts = [...document.querySelectorAll(".text-gray-500")]
         .filter(e => e.innerText.startsWith("Level "))
         .map((e, i) => {
@@ -39,23 +57,6 @@ async function sortCharacters() {
                         a.positionChange > b.positionChange ? 1 :
                         0
              );
-
-    const THRESHOLD_LOG_CACHE_LIMIT = 2;
-    let thresholdLogCache = [];
-    function tLog(...args) {
-        if(thresholdLogCache?.length < THRESHOLD_LOG_CACHE_LIMIT) {
-            thresholdLogCache.push(args);
-            return;
-        }
-        if(thresholdLogCache?.length >= THRESHOLD_LOG_CACHE_LIMIT) {
-            for(let cachedArgs of thresholdLogCache) {
-                console.log(...cachedArgs);
-            }
-            thresholdLogCache = undefined;
-        }
-        console.log(...args);
-    }
-    function restoreTLogCache() { thresholdLogCache = []; }
 
     function nextMove(name, startLevel, endLevel, expectedIndex) {
         tLog(name, startLevel, "to", expectedIndex);
@@ -106,104 +107,119 @@ function sortTodoListItems() {
       , "Adventurer's Experience": 88_000_000_000
       , "Wanderer's Advice":       87_000_000_000
 
+        // Pyro
       , "Agnidus Agate Gemstone": 79_900_000_000
       , "Agnidus Agate Chunk":    79_800_000_000
       , "Agnidus Agate Fragment": 79_700_000_000
       , "Agnidus Agate Sliver":   79_600_000_000
       , "Smoldering Pearl":       79_500_000_000        // Cube
-      , "Everflame Seed":         79_400_000_000            // Plant
-
+      , "Everflame Seed":         79_400_000_000        // Plant
+       
+        // Hydro
       , "Varunada Lazurite Gemstone": 78_900_000_000
       , "Varunada Lazurite Chunk":    78_800_000_000
       , "Varunada Lazurite Fragment": 78_700_000_000
       , "Varunada Lazurite Sliver":   78_600_000_000
       , "Dew of Repudiation":         78_500_000_000    // Cube
-        // Plant
-      , "Cleansing Heart":            78_300_000_000            // Oceanid
-
+                                                        // Plant
+      , "Cleansing Heart":            78_300_000_000    // Oceanid
+       
+        // Dendro
       , "Nagadus Emerald Gemstone": 77_900_000_000
       , "Nagadus Emerald Chunk":    77_800_000_000
       , "Nagadus Emerald Fragment": 77_700_000_000
       , "Nagadus Emerald Sliver":   77_600_000_000
       , "Quelled Creeper":          77_500_000_000      // Cube
-        // Plant
-      , "Majestic Hooked Beak":     77_300_000_000              // Bird
-
+                                                        // Plant
+      , "Majestic Hooked Beak":     77_300_000_000      // Bird
+       
+        // Electro
       , "Vajrada Amethyst Gemstone": 76_900_000_000
       , "Vajrada Amethyst Chunk":    76_800_000_000
       , "Vajrada Amethyst Fragment": 76_700_000_000
       , "Vajrada Amethyst Sliver":   76_600_000_000
       , "Lightning Prism":           76_500_000_000     // Cube
-      , "Thunderclap Fruitcore":     76_400_000_000         // Plant
-      , "Storm Beads":               76_300_000_000             // Bird
-
+      , "Thunderclap Fruitcore":     76_400_000_000     // Plant
+      , "Storm Beads":               76_300_000_000     // Bird
+       
+        // Anemo
       , "Vayuda Turquoise Gemstone": 75_900_000_000
       , "Vayuda Turquoise Chunk":    75_800_000_000
       , "Vayuda Turquoise Fragment": 75_700_000_000
       , "Vayuda Turquoise Sliver":   75_600_000_000
       , "Hurricane Seed":            75_500_000_000     // Cube
-
+                                                        // Plant
+       
+        // Cryo
       , "Shivada Jade Gemstone": 74_900_000_000
       , "Shivada Jade Chunk":    74_800_000_000
       , "Shivada Jade Fragment": 74_700_000_000
       , "Shivada Jade Sliver":   74_600_000_000
       , "Crystalline Bloom":     74_500_000_000         // Cube
-      , "Hoarfrost Core":        74_400_000_000             // Plant
-
+      , "Hoarfrost Core":        74_400_000_000         // Plant
+       
+        // Geo
       , "Prithiva Topaz Gemstone": 73_900_000_000
       , "Prithiva Topaz Chunk":    73_800_000_000
       , "Prithiva Topaz Fragment": 73_700_000_000
       , "Prithiva Topaz Sliver":   73_600_000_000
       , "Basalt Pillar":           73_500_000_000       // Cube
-        // Plant
-      , "Runic Fang":              73_300_000_000               // Ruin Serpent
+                                                        // Plant
+      , "Runic Fang":              73_300_000_000       // Ruin Serpent
 
+        // Mixed Bosses
       , "Juvenile Jade":             72_900_000_000
       , "Marionette Core":           72_800_000_000
       , "Perpetual Heart":           72_700_000_000
       , "Perpetual Caliber":         72_600_000_000
       , "Light Guiding Tetrahedron": 72_500_000_000
 
+        // Slime
       , "Slime Concentrate": 69_900_000_000
       , "Slime Secretions":  69_800_000_000
       , "Slime Condensate":  69_700_000_000
 
+        // All Hilichurls
       , "Ominous Mask": 68_900_000_000
       , "Stained Mask": 68_800_000_000
       , "Damaged Mask": 68_700_000_000
 
+        // Samachurls (Staff mages)
       , "Forbidden Curse Scroll": 67_900_000_000
       , "Sealed Scroll":          67_800_000_000
       , "Divining Scroll":        67_700_000_000
 
+        // Hilichurl Shooters
       , "Weathered Arrowhead": 66_900_000_000
       , "Sharp Arrowhead":     66_800_000_000
       , "Firm Arrowhead":      66_700_000_000
 
+        // Mitachurls and Lawachurls (Big Bois)
       , "Black Crystal Horn": 65_900_000_000
       , "Black Bronze Horn":  65_800_000_000
       , "Heavy Horn":         65_700_000_000
 
+        // Abyss Mages (Shielded mages) & Abyss Lectors and Heralds
       , "Ley Line Sprout":      64_900_000_000
       , "Dead Ley Line Leaves": 64_800_000_000
       , "Dead Ley Line Branch": 64_700_000_000
-
+        // Humanoid Ruined Machines
       , "Chaos Core":    63_900_000_000
       , "Chaos Circuit": 63_800_000_000
       , "Chaos Device":  63_700_000_000
-
+        // Fatui Cicin Mages
       , "Mist Grass Wick":   62_900_000_000
       , "Mist Grass":        62_800_000_000
       , "Mist Grass Pollen": 62_700_000_000
-
+        // Fatui Agents
       , "Inspector's Sacrificial Knife": 61_900_000_000
       , "Agent's Sacrificial Knife":     61_800_000_000
       , "Hunter's Sacrificial Knife":    61_700_000_000
-
+        // Fatui enemies (except Mirror Maiden)
       , "Lieutenant's Insignia": 60_900_000_000
       , "Sergeant's Insignia":   60_800_000_000
       , "Recruit's Insignia":    60_700_000_000
-
+        
       , "Golden Raven Insignia":     59_900_000_000
       , "Silver Raven Insignia":     59_800_000_000
       , "Treasure Hoarder Insignia": 59_700_000_000
@@ -282,7 +298,7 @@ function insertSortButtons() {
     rowContainer.classList.add("flex", "items-center", "justify-center", "mb-2", "gap-1");
 
     const makeButton = (innerText, clickHandler) => {
-        let button = document.createElement("div");
+        let button = document.createElement("button");
         button.classList.add("text-white",
                              "border-2", "border-white", "border-opacity-25",
                              "rounded-xl",
@@ -294,6 +310,7 @@ function insertSortButtons() {
         button.addEventListener("click", async () => {
             try {
                 button.disabled = true;
+                await sleep(0);
                 await clickHandler();
             } finally {
                 button.disabled = false;
