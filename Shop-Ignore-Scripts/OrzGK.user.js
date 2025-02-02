f804c0f23c90d2173b830185e8d04ce20270c320
Adowrath <adowrath@protonmail.ch>
2025-02-02 22:47:40 +0100

    Update OrzGK.user.js

--- Shop-Ignore-Scripts/OrzGK.user.js
+++ Shop-Ignore-Scripts/OrzGK.user.js
@@ -56,7 +56,7 @@ const routes = [
         ...titleChanger(() => `[Product: TODO] ${document.title}`),
         menuItems: [{
             text: "Ignore and Close",
-            async action(cb) {
+            async action(event, cb) {
                 let ignoreBtn = document.querySelector(".btn-ignore");
 
                 while(true) {
