ddd2cc992f9c4cb2be4d1b5bccca7d88406ff0d9
Adowrath <adowrath@protonmail.ch>
2025-03-10 15:39:33 +0100

    Update OrzGK.user.js

--- Shop-Ignore-Scripts/OrzGK.user.js
+++ Shop-Ignore-Scripts/OrzGK.user.js
@@ -20,12 +20,17 @@ const routes = [
             /^\/ostatus\//,
             /^\/brand\//,
         ],
-        ...titleChanger(() => {
-            let resultElem = document.querySelector(".woocommerce-result-count");
-            if(resultElem) {
-                let total = /^Showing \d+–\d+ of (\d+) results$/.exec(resultElem.innerText)?.[1];
+        ...titleChanger(async () => {
+            while(true) {
+                let resultElem = document.querySelector(".woocommerce-result-count");
+                if(resultElem) {
+                    let resultText = resultElem.textContent.trim();
+                    let total = /^Showing \d+–\d+ of (\d+) results/.exec(resultText)?.[1]
+                    ?? /^Showing all (\d+) results/.exec(resultText)?.[1];
 
-                return `[Search: ${formatNumber(total)}] ${document.title}`;
+                    return `[Search: ${formatNumber(total)}] ${document.title}`;
+                }
+                await sleep(1000);
             }
         }),
         menuItems: [{
@@ -34,11 +39,31 @@ const routes = [
                 let productElems = [...document.querySelectorAll(".col-inner .product-small")];
 
                 let unprocessed = () => productElems.filter(e => !e.classList.contains("processed"));
+                let loadMore = () => [...document.querySelectorAll(".fibofilters-show-more")].filter(e => e.textContent.trim() === "Show more products");
                 cb(`Check and Close: ${unprocessed().length}`);
 
                 if(event.ctrlKey) {
-                    while(unprocessed().length > 0) {
-                        await sleep(100);
+                    while(true) {
+                        while(unprocessed().length > 0) {
+                            await sleep(100);
+                        }
+                        let loadMoreButtons = loadMore();
+                        if(loadMoreButtons.length > 0) {
+                            if(location.pathname.indexOf("/page/") !== -1) {
+                                location.href = location.href.replace(/\/page\/(\d+)\//, (_, page) => `/page/${+page + 1}/`);
+                            } else {
+                                let lastLocation = location.href;
+                                loadMoreButtons[0].click();
+                                while(document.querySelector(".fibofilters-product-placeholder") !== null) {
+                                    await sleep(100);
+                                }
+                                while(lastLocation === location.href) {
+                                    await sleep(100);
+                                }
+                                continue;
+                            }
+                        }
+                        break;
                     }
                 }
 
