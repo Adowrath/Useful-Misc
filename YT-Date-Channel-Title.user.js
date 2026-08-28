444dd2ee0cbeeb16b8b4c2c3b460acbeaece1461
Adowrath <adowrath@protonmail.ch>
2026-08-28 10:56:14 +0200

    Add TrustedTypes support so script can parse document again

--- YT-Date-Channel-Title.user.js
+++ YT-Date-Channel-Title.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Date and Channel in Title
 // @namespace    http://tampermonkey.net/
-// @version      v1.2
+// @version      v1.3
 // @description  Adds Date and Channel to the tab's title
 // @author       Adowrath
 // @match        https://www.youtube.com/*
@@ -13,24 +13,53 @@
 (async function() {
     'use strict';
 
-    function findTitle() {
-        let videoObject = document.querySelector(`div[itemtype="http://schema.org/VideoObject"]`);
-        if(videoObject === null) return "";
+    const nonePolicy = trustedTypes.createPolicy("none", {
+        createHTML: (string) => string,
+    });
+    let parser = new DOMParser();
 
-        let datePublished = videoObject.querySelector(`[itemprop="datePublished"]`).content;
-        let uploadDate = videoObject.querySelector(`[itemprop="uploadDate"]`).content;
-        let channel = videoObject.querySelector(`[itemprop="author"] [itemprop="name"]`).getAttribute("content");
-        let title = videoObject.querySelector(`[itemprop="name"]`).content;
+    async function findTitle() {
+        let docContent = await (await fetch(location.href)).text();
+        let docHtml = nonePolicy.createHTML(docContent);
+        let doc = parser.parseFromString(docHtml, "text/html");
 
-        if(datePublished !== uploadDate) throw new Error("Incompatible dates");
-        let date = datePublished.split("T")[0];
+        let channel;
+        let date;
+        let title;
+
+        let videoObject = doc.querySelector(`div[itemtype*="http://schema.org/VideoObject"]`);
+        if(videoObject === null) {
+            /*
+            let schemaElements =
+                [...doc.querySelectorAll("*[type=\"application/ld+json\"]")]
+                    .filter(e => e.textContent.indexOf(`{"@context":"https://schema.org"`) !== -1);
+            if(schemaElements.length !== 1) {
+                return "";
+            }
+
+            let schema = JSON.parse(schemaElements[0].textContent);
+
+            channel = "?Unknown?";
+            date = schema.uploadDate.split("T")[0];
+            title = schema.name;
+            */
+            return "";
+        } else {
+            let datePublished = videoObject.querySelector(`[itemprop="datePublished"]`).content;
+            let uploadDate = videoObject.querySelector(`[itemprop="uploadDate"]`).content;
+
+            if(datePublished !== uploadDate) throw new Error("Incompatible dates");
+
+            channel = videoObject.querySelector(`[itemprop="author"] [itemprop="name"]`).getAttribute("content");
+            date = datePublished.split("T")[0];
+            title = videoObject.querySelector(`[itemprop="name"]`).content;
+        }
 
         return `[${date}] - [${channel}] - ${title}`;
     }
 
     const sleep = ms => new Promise(r => setTimeout(r, ms));
 
-    await sleep(1000 * 3);
     while(true) {
         await sleep(100);
         let loc = location.href;
@@ -41,8 +70,11 @@
         }
         let title;
         try {
-            title = findTitle();
-            if(title === "") continue;
+            title = await findTitle();
+            if(title === "") {
+                await sleep(1000);
+                continue;
+            }
         } catch(e) {
             console.error(e);
             alert("Error!");
